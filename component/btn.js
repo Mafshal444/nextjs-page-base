331517118a06b6983582831a25b4a30547e89fca
Mafshal444 <112098761+Leomafshal@users.noreply.github.com>
2023-09-18 15:24:15 +0500

    first commit

---
+++ component/btn.js
@@ -0,0 +1,12 @@
+import 'reach/button.css'
+export default function Bton() {
+  return (
+    <div>
+        <h1>Hi</h1>
+        <button className='button1'>Start</button>
+        <button id="button2">Start</button>
+        <button >Start</button>
+        <button className="button3">Start</button>
+    </div>
+  )
+}
